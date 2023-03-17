f29cc01862ba893070b361326540ff4902da44e8
Pedro Ferreira <ferreirapedro.sjm@gmail.com>
2023-03-17 18:56:47 +0000

    otimized folder structure, created profile page and redirect user to profile page after loggin in

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { AuthGuard } from "./auth.guard";
+import { HomeComponent } from "./components/home/home.component";
+import { LoginComponent } from "./components/login-page/login-page.component";
+import { ProfilePageComponent } from "./pages/profile-page/profile-page.component";
+
+
+const routes: Routes = [
+    { 
+        path: "home", 
+        component: HomeComponent, 
+        canActivate: [AuthGuard] 
+    },
+    { 
+        path: "login", 
+        component: LoginComponent 
+    },
+    { 
+        path: "profile", 
+        component: ProfilePageComponent, 
+        canActivate: [AuthGuard] 
+    },
+    { 
+        path: "", 
+        redirectTo: "login", 
+        pathMatch: "full"
+    },
+    { 
+        path: "**", 
+        redirectTo: "login", 
+        pathMatch: "full"
+    }
+  ];
+
+  
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    providers: [AuthGuard],
+    exports: [RouterModule],
+  })
+  export class AppRoutingModule {}
+  
