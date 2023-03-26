f405f1f553e828d3a47d17c3c25814924b13ace5
Pedro Ferreira <ferreirapedro.sjm@gmail.com>
2023-03-26 22:09:25 +0100

    started cost calculations for the shipment

--- src/app/components/list.order/list.order.component.ts
+++ src/app/components/list.order/list.order.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { Countries } from 'src/app/enums/Countries.enum';
+import { COUNTRY_MULTIPLIERS } from 'src/app/models/Countries';
 import { GetterShipment } from 'src/app/models/GetterShipment';
 import { Shipment } from 'src/app/models/Shipment';
 import { ShipmentService } from 'src/app/services/shipment.service';
@@ -19,7 +22,7 @@ export class ListOrderComponent implements OnInit {
 
     ngOnInit() {
       this.shipmentService.getAllShipments().subscribe(shipments => {
-        this.shipments = shipments;
+      this.shipments = shipments;
       });
     
       this.orderModal.shipmentCreated.subscribe((shipment: GetterShipment) => {
@@ -27,7 +30,28 @@ export class ListOrderComponent implements OnInit {
       });      
     }
     
-  calculateCost(shipment: GetterShipment) {
-    // logic to calculate the cost based on the weight and destination of the shipment
-  }
+    calculateCost(weight: string, country: string): Observable<number> {
+      return this.shipmentService.getAllShipments().pipe(
+        map(shipments => {
+          const shipment = shipments.find(s => s.destination === country);
+          if (!shipment) {
+            throw new Error(`No shipment found for country: ${country}`);
+          }
+          let costCountry: string = shipment.destination;
+          const countryMultiplier = COUNTRY_MULTIPLIERS[costCountry];
+          let weightInKg = 0;
+          if (weight === "BASIC") {
+            weightInKg = 1;
+          } else if (weight === "HUMBLE") {
+            weightInKg = 2;
+          } else if (weight === "DELUXE") {
+            weightInKg = 5;
+          } else if (weight === "PREMIUM") {
+            weightInKg = 8;
+          }
+          return 200 + weightInKg * countryMultiplier;
+        })
+      );
+    }
+    
 }
